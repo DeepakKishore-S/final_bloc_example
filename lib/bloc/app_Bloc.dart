b9c94b567749fc78dc313752b03e1f4dad034ad4
Deepak Kishore S <sdkdeepak600@gmail.com>
2023-05-11 07:05:08 +0530

    dlete account bloc

--- lib/bloc/app_Bloc.dart
+++ lib/bloc/app_Bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:final_bloc_example/auth/auth_error.dart';
 import 'package:final_bloc_example/bloc/app_event.dart';
 import 'package:final_bloc_example/bloc/app_state.dart';
 import 'package:final_bloc_example/utils/upload_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,6 +14,55 @@ class AppBloc extends Bloc<AppEvent, AppState> {
           isLoading: false,
           authError: null,
         )) {
+    on<AppEventDeleteAccount>(
+      (event, emit) async {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user == null) {
+          emit(const AppStateLoggedOut(
+            isLoading: false,
+            authError: null,
+          ));
+          return;
+        }
+        emit(AppStateLoggedIn(
+          user: user,
+          images: state.images ?? [],
+          isLoading: true,
+          authError: null,
+        ));
+
+        try {
+          final foldercontents =
+              await FirebaseStorage.instance.ref(user.uid).listAll();
+          for (final item in foldercontents.items) {
+            await item.delete().catchError((_) {});
+          }
+          await FirebaseStorage.instance
+              .ref(user.uid)
+              .delete()
+              .catchError((_) {});
+          await user.delete();
+          await FirebaseAuth.instance.signOut();
+          emit(const AppStateLoggedOut(
+            isLoading: false,
+            authError: null,
+          ));
+        } on FirebaseAuthException catch (e) {
+          emit(AppStateLoggedIn(
+            user: user,
+            images: state.images ?? [],
+            isLoading: false,
+            authError: AuthError.from(e),
+          ));
+        } on FirebaseException {
+          emit(const AppStateLoggedOut(
+            isLoading: false,
+            authError: null,
+          ));
+        }
+      },
+    );
+
     on<AppEventUploadImage>((
       event,
       emit,
