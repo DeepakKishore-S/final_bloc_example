6de90aba9ebc1de3ae320bc4a7bb46a468decbcd
Deepak Kishore S <sdkdeepak600@gmail.com>
2023-06-10 16:03:00 +0530

    new 2

--- lib/bloc/app_Bloc.dart
+++ lib/bloc/app_Bloc.dart
@@ -22,6 +22,7 @@ class AppBloc extends Bloc<AppEvent, AppState> {
         if (user == null) { 
           
           emit(
+            
             const AppStateLoggedOut(
               "test",
               isLoading: false,
